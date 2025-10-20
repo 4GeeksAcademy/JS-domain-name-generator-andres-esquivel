84d1a4e9549c729193ee17bbab92e63493cb795f
AEGiner <andresesquivel2011@gmail.com>
2025-10-20 23:58:59 +0000

    fixed file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let domain = ['.com', '.net', '.us']
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function randomDomain(arr1, arr2, arr3, arr4) {
+    for (let i in arr1) {
+        for (let a in arr2) {
+            for (let n in arr3) {
+                for (let d in arr4) {
+                    console.log(`${arr1[i]}${arr2[a]}${arr3[n]}${arr4[d]}`)
+                }
+            }
+        }
+    }
+}
+randomDomain(pronoun, adj, noun,domain);
